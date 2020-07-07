9f39cf7439cb034a20462200fab0747c46c751ec
Trinh Duc Thinh <trinhthinh388@gmail.com>
2020-07-07 10:50:41 +0700

    feat update item from list

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,22 +1,34 @@
-import React, {useState, useEffect} from 'react';
+import React, {useRef} from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faTimes, faCheck} from '@fortawesome/free-solid-svg-icons';
 import classNames from "classnames";
 
-export default function TodoItem(props){
-    const [check, setCheck] = useState(props.checked);
-
-    function onClickHandle(e){
-        setCheck(!check);
-        console.log(check);
+export default class TodoItem extends React.Component{
+    constructor(props){
+        super(props);
     }
-    return(
-        <div className="mx-auto list__item d-flex flex-row align-items-center col-lg-6">
-            <div onClick={onClickHandle}  className={classNames("item__circle-check", {"circle--focus": check})}>
-                <FontAwesomeIcon className={classNames({"check--display": check})} icon={faCheck} color="#28A745" size="2x"/>
-            </div>
-            <div className="item__content">{props.content}</div>
-            <FontAwesomeIcon className="item__delete" icon={faTimes} color="#DC3545" size="2x"/>
-        </div>
-    );
+
+    onClickHandle =()=>{
+        this.props.UpdateItem(null, !this.props.check, this.props.index);
+    }
+
+    onInputChangeHandle =(e)=>{
+        let value = e.currentTarget.value;
+        this.props.UpdateItem(value, false, this.props.index);
+    }
+
+    render(){
+        const {check, content, index} = this.props;
+        return(
+            <div className="mx-auto list__item d-flex flex-row align-items-center col-lg-6">
+                <div onClick={this.onClickHandle}  className={classNames("item__circle-check", {"circle--focus": check})}>
+                    <FontAwesomeIcon className={classNames({"check--display": check})} icon={faCheck} color="#28A745" size="2x"/>
+                </div>
+                <div className="item__content">
+                    <input onChange={this.onInputChangeHandle} defaultValue={content}></input>
+                </div>
+                <FontAwesomeIcon className="item__delete" icon={faTimes} color="#DC3545" size="2x"/>
+             </div>
+        );
+    }
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import TodoItem from './TodoItem';
+import generateHash from 'random-hash';
 
 export default function TodoList(props){
+
+    const {list, UpdateList} = props;
+
+    function updateItem(content, done, index){
+        let _list = list.slice();
+        _list.forEach((item, _index) =>{
+            if(_index === index){
+                if(content)
+                    item.content = content;
+                item.done = done;
+                return;
+            }
+        });
+        UpdateList(_list);
+    }
+    
+    function deleteItem(content, index){
+
+    }
 
     return(
         <div className="todo__list container">
             <div className="row flex-column justify-content-center">
                 {
-                    props.list.map(item => {
-                        return <TodoItem content={item.content} check={item.done}/>
+                    props.list.map((item, index) => {
+                        return <TodoItem key={`todo-item-${index}`} index={index} UpdateItem={updateItem} content={item.content} check={item.done}/>
                     })
                 }
             </div>
